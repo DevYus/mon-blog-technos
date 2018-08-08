5ff1a33c5c2771a10344272174a90f577fd390f5
Dev@Yus <miroball95@gmail.com>
2018-08-08 15:14:02 +0200

    Test nouveau commit

---
+++ src/My/TechnosBlogBundle/Controller/Front/RedirectAfterLoginController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoi
+ * Date: 06/08/2018
+ * Time: 12:15
+ */
+
+namespace My\TechnosBlogBundle\Controller\Front;
+
+
+class RedirectAfterLoginController
+{
+
+}
---
+++ src/My/TechnosBlogBundle/Form/ForgotPasswordType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yoi
+ * Date: 08/08/2018
+ * Time: 12:24
+ */
+
+namespace My\TechnosBlogBundle\Form;
+
+
+class ForgotPasswordType
+{
+
+}
