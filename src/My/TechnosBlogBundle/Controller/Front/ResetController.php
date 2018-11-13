fe387713d567b62cbd9709316e4a3cbde7f296a1
Dev@Yus <miroball95@gmail.com>
2018-11-13 15:48:56 +0100

    clean code 27

--- src/My/TechnosBlogBundle/Controller/Front/ResetController.php
+++ src/My/TechnosBlogBundle/Controller/Front/ResetController.php
@@ -3,10 +3,8 @@
 namespace My\TechnosBlogBundle\Controller\Front;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use My\TechnosBlogBundle\Form\ResetPasswordType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
 
 
 /**
