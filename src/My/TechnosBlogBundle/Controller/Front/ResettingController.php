e07d91d320a438960e4edacc809a2a96853eddef
Dev@Yus <miroball95@gmail.com>
2018-11-13 16:04:57 +0100

    clean code 35

--- src/My/TechnosBlogBundle/Controller/Front/ResettingController.php
+++ src/My/TechnosBlogBundle/Controller/Front/ResettingController.php
@@ -5,7 +5,7 @@ namespace My\TechnosBlogBundle\Controller\Front;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
- * Class ResetController
+ * Class ResettingController
  * @package My\TechnosBlogBundle\Controller\Front
  */
 class ResettingController extends Controller
