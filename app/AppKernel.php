c64630bf261241416b06e567e675c61e23fe5ea6
Dev@Yus <miroball95@gmail.com>
2018-11-13 13:25:19 +0100

    test for correction 8

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -49,6 +49,11 @@ class AppKernel extends Kernel
         return dirname(__DIR__).'/var/logs';
     }
 
+    public function getCharset()
+    {
+        return 'UTF-8';
+    }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(function (ContainerBuilder $container) {
