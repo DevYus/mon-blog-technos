90b532cca434dd5ed823c3db18fe73f98f2cb63a
Dev@Yus <miroball95@gmail.com>
2018-11-13 13:17:22 +0100

    test for correction 6

--- src/My/TechnosBlogBundle/Controller/Back/AddAdministratorController.php
+++ src/My/TechnosBlogBundle/Controller/Back/AddAdministratorController.php
@@ -18,48 +18,6 @@ use My\TechnosBlogBundle\Form\UsersType;
 
 class AddAdministratorController extends Controller
 {
-    /**
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $encoder
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
-     */
-    public function addAdministratorAction(Request $request, UserPasswordEncoderInterface $encoder)
-    {
-        $user = new Users();
-        $form = $this->get('form.factory')->create(UsersType::class, $user);
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            // Encode password of the user
-            $password = $encoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
-
-            // Set default token
-            $token = bin2hex(openssl_random_pseudo_bytes(16));
-            $user->setResetToken($token);
-            $user->setResetExpires(0);
-
-            // Set default role user
-            $user->setRoles(['ROLE_ADMIN']);
-
-            // Flush to database
-            $entMa = $this->getDoctrine()->getManager();
-            $entMa->persist($user);
-            $entMa->flush();
-
-            $request->getSession()->getFlashBag()->add('notice', 'Un nouvel administrateur à été crée');
-
-            return $this->redirectToRoute('admin_all_users');
-        }
-
-        return $this->render('@MyTechnosBlog/Back/AddAdministrator\addAdministrator.html.twig',
-            [
-                'form' => $form->createView()
-            ]);
-
-    }
 
 
 }
--- src/My/TechnosBlogBundle/Controller/Front/RegistrationController.php
+++ src/My/TechnosBlogBundle/Controller/Front/RegistrationController.php
@@ -17,44 +17,5 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class RegistrationController extends Controller
 {
 
-    /**
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $encoder
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     */
 
-    public function registrationAction(Request $request, UserPasswordEncoderInterface $encoder)
-    {
-        $user = new Users();
-        $form = $this->get('form.factory')->create(UsersType::class, $user);
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid())
-        {
-            // Encode password of the user
-            $password = $encoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($password);
-
-            // Set default token
-            $token = bin2hex(openssl_random_pseudo_bytes(16));
-            $user->setResetToken($token);
-            $user->setResetExpires(0);
-
-            // Set default role user
-            $user->setRoles(['ROLE_USER']);
-
-            // Flush to database
-            $entMa = $this->getDoctrine()->getManager();
-            $entMa->persist($user);
-            $entMa->flush();
-
-            return $this->redirectToRoute('login');
-        }
-
-        return $this->render(
-            '@MyTechnosBlog/Front/Registration\registration.html.twig',
-            [ 'form' => $form->createView()]
-        );
-    }
 }
