d1ebd89a5fe0e9092e3714b32667f01e15c56975
Dev@Yus <miroball95@gmail.com>
2018-11-13 15:46:27 +0100

    clean code 26

--- src/My/TechnosBlogBundle/Controller/Front/ResetController.php
+++ src/My/TechnosBlogBundle/Controller/Front/ResetController.php
@@ -5,7 +5,7 @@ namespace My\TechnosBlogBundle\Controller\Front;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use My\TechnosBlogBundle\Form\ResetPasswordType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Reponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
@@ -19,7 +19,7 @@ class ResetController extends Controller
      public function resetAction(Request $request, UserPasswordEncoderInterface $encoder)
      {
 
-         
+
 
      }
 
