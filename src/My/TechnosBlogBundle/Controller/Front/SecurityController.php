9bc2886671f356f200159800807fc35691796c24
Dev@Yus <miroball95@gmail.com>
2018-11-13 16:59:12 +0100

    clean code 44

---
+++ src/My/TechnosBlogBundle/Controller/Front/SecurityController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace My\TechnosBlogBundle\Controller\Front;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * Class SecurityController
+ * @package My\TechnosBlogBundle\Controller\Front
+ */
+
+class SecurityController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return Response
+     */
+
+    public function loginAction(Request $request)
+    {
+        if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+        {
+            return $this->redirectToRoute('login');
+        }
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+
+        return $this->render('@MyTechnosBlog/Front/Security\login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    /**
+     * @throws \Exception
+     */
+
+    public function logout()
+    {
+        throw new \Exception('Ne dois jamais être executé');
+    }
+}
