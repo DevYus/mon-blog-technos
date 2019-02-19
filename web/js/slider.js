432225ca2bb402164c1571ce39f96b704ec0c756
Dev@Yus <miroball95@gmail.com>
2019-02-19 14:48:05 +0100

    Slider in progress 75%

--- web/js/slider.js
+++ web/js/slider.js
@@ -9,30 +9,79 @@ class Slider {
      */
     constructor (element, options = {}) {
         this.element = element;
-        this.children  = [].slice.call(element.children);
         this.options = Object.assign({}, {
-            numberSlidesNext : 3,
-            numberSlidesVisible : 3,
+            numberSlidesNext : 1,
+            numberSlidesVisible : 1,
         }, options);
 
-        let maxItem = this.children.length / this.options.numberSlidesVisible;
+        let children  = [].slice.call(element.children);
 
-        let wrapper = this.createWrapperAndClass('slider');
-        let subContainer = this.createWrapperAndClass('subContainer');
+        this.wrapper = this.createWrapperAndClass('slider');
+        this.subContainer = this.createWrapperAndClass('subContainer');
 
-        subContainer.style.width = (maxItem * 100) + "%";
+        this.wrapper.appendChild(this.subContainer);
+        this.element.appendChild(this.wrapper);
 
-        wrapper.appendChild(subContainer);
-        this.element.appendChild(wrapper);
-
-        this.children.forEach((elements) => {
+        this.items = children.map((elements) => {
             let item = this.createWrapperAndClass('sliderItem');
-            item.style.width = ((100 / this.options.numberSlidesVisible) /maxItem) +  "%";
             item.appendChild(elements)
-            subContainer.appendChild(item);
+            this.subContainer.appendChild(item);
+            return item;
         });
 
+        this.cssForResponsive();
+
+        this.currentSlide = 0;
+        this.createNav();
+
     }
+
+    /**
+     * Css according to resolution screen
+     */
+    cssForResponsive () {
+
+        let maxItem = this.items.length / this.options.numberSlidesVisible;
+        this.subContainer.style.width = (maxItem * 100) + "%";
+        this.items.forEach(item => item.style.width = (( 100 / this.options.numberSlidesVisible ) / maxItem) +  "%");
+    }
+
+    /**
+     *  Creation and navigation in the slider
+     */
+    createNav () {
+
+        let prevBtn = this.createWrapperAndClass('sliderPrev');
+        let nextBtn = this.createWrapperAndClass('sliderNext');
+
+        this.wrapper.appendChild(prevBtn);
+        this.wrapper.appendChild(nextBtn);
+
+        prevBtn.addEventListener('click', this.prev.bind(this));
+        nextBtn.addEventListener('click', this.next.bind(this));
+    }
+
+
+    prev () {
+        this.goToSlide(this.currentSlide - this.options.numberSlidesNext);
+    }
+
+    next () {
+        this.goToSlide(this.currentSlide + this.options.numberSlidesNext);
+    }
+
+    /**
+     *
+     * @param {number} index
+     */
+    goToSlide (index) {
+
+        (index < 0) ? index = this.items.length - this.options.numberSlidesVisible : (index >= this.items.length) ?  index = 0  : false;
+
+        let translateX = index * - 100 / this.items.length;
+        this.subContainer.style.transform = 'translate3d(' + translateX + '%,0,0)';
+        this.currentSlide = index;
+    };
 
     /**
      *
@@ -52,8 +101,8 @@ class Slider {
 document.addEventListener('DOMContentLoaded', function(){
 
     new Slider(document.querySelector("#slider-1"), {
-        numberSlidesNext : 3,
-        numberSlidesVisible : 3,
+        numberSlidesNext : 1,
+        numberSlidesVisible : 1,
     });
 
 });
